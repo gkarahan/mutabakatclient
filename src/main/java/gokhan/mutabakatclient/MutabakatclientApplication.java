cc4ec74a6c32c7b57e279f1e35bef3aa88e89a1c
gkarahan <karahan.gokhan@gmail.com>
2022-08-13 14:04:15 +0300

    Initial commit

---
+++ src/main/java/gokhan/mutabakatclient/MutabakatclientApplication.java
@@ -0,0 +1,13 @@
+package gokhan.mutabakatclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MutabakatclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MutabakatclientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gokhan/mutabakatclient/MutabakatclientApplicationTests.java
@@ -0,0 +1,13 @@
+package gokhan.mutabakatclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MutabakatclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
